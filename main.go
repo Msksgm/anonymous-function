dac30be350c752c46ae102a5ad4c6412732d0e06
Msksgm <msksgm3124@gmail.com>
2022-02-05 15:19:15 +0900

    fix

--- main.go
+++ main.go
@@ -6,22 +6,27 @@ import (
 )
 
 func throwErr(flag bool) error {
+	fmt.Println("throwErr")
 	if flag {
-		return fmt.Errorf("err is occured")
+		return fmt.Errorf("err is occured in throwErr")
 	} else {
 		return nil
 	}
 }
 
-func returnErr(flag bool) (err error) {
-	defer func() {
-		err = throwErr(flag)
+func returnErr(flag bool) error {
+	fmt.Println("returnErr")
+	defer func() error {
+		err := throwErr(flag)
+		return err
 	}()
 	return nil
 }
 
 func main() {
+	fmt.Println("main function is start")
 	if err := returnErr(true); err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("main function is end")
 }
